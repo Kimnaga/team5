0a678942326f842dfeea837d7ce392d993215584
wenjun weng <rebecca1991123@gmail.com>
2015-10-01 15:16:47 -0500

    gift changed rebecca

--- src/com/se452/UserGift/Gift.java
+++ src/com/se452/UserGift/Gift.java
@@ -11,6 +11,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
@@ -23,38 +24,52 @@ import org.eclipse.persistence.internal.jpa.parsing.TemporalLiteralNode.Temporal
 import sun.util.calendar.LocalGregorianCalendar.Date;
 
 @Entity
-@Table(name="USERGIFT")
+@Table(name="GIFT")
+@SequenceGenerator(name="Gift_seq", initialValue=1,allocationSize=1)
 public class Gift {
+	
+	
 	@Id
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="User_seq")
-	@Column(name = "GIFT")
-	private int GiftId;
-	@Column(name="GIFTDESCIRPTION",nullable=false)
-	private String GiftDescription;
-	@Column(name="GiftMessage",nullable=false)
-	private String GiftMessage;
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="Gift_seq")
+	private int giftId;
 	
-
-	public int getGift_id() {
-		return GiftId;
+	@OneToMany(cascade=CascadeType.ALL, mappedBy="giftId")
+	private List<UserGift> usergift ;
+	@Column(name="Gift_Description")
+	private String giftDescription;
+	@Lob
+	@Column(name="PICTURE")
+	private byte[] giftPicture;
+	@Column(name="Gift_Name",unique=true,length=30)
+	private String giftName;
+	
+	public String getGiftName() {
+		return giftName;
 	}
-	public void seGift_id(int gift_id) {
-		this.GiftId = GiftId;
+	public void setGiftName(String giftName) {
+		this.giftName = giftName;
 	}
-
+	public int getGiftId() {
+		return giftId;
+	}
+	public void setGiftId(int giftId) {
+		this.giftId = giftId;
+	}
+	 
 	public String getGiftDescription() {
-		return GiftDescription;
+		return giftDescription;
 	}
+	public void setGiftDescription(String giftDescription) {
+		this.giftDescription = giftDescription;
+	}
+	public byte[] getGiftPicture() {
+		return giftPicture;
+	}
+	public void setGiftPicture(byte[] giftPicture) {
+		this.giftPicture = giftPicture;
+	}
+
 	
-	public void setGiftDescription(String GfitDescription) {
-		this.GiftDescription = GiftDescription;
-	}
+
 	
-	public String GiftMessage() {
-		return GiftDescription;
-	}
-	
-	public void setGiftMessage(String GiftMessage) {
-		this.GiftDescription = GiftDescription;
-	}
 }
